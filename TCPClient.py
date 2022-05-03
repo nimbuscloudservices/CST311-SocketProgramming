c3104a07a6f1e49b0b0064aa044d15da7a91a4c0
Layla Gallez <layla.gallez@gmail.com>
2022-05-02 21:27:56 -0700

    Updated error

--- TCPClient.py
+++ TCPClient.py
@@ -4,7 +4,7 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 sentence = input('Input lowercase sentence: ')
-clientSocket.sendto(sentence.encode())
+clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024)
 print('From Server: ', modifiedSentence.decode())
 clientSocket.close()
